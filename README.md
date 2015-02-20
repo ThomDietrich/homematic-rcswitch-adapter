48c970b159aa02fa1c5fe7fb21c7ab0804eb2a98
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2015-02-20 17:29:04 +0100

    initial working version, README + image

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# homematic-rcswitch-adapter
-HomeMatic Funk-Schaltsteckdosen Adapter, Arduino Nano, HomeMatic 8-Kanal-Empfangsmodul
+# HomeMatic Funk-Schaltsteckdosen Adapter
+
+Eine Arduino Schaltung und Programmierung um über ein HomeMatic 8-Kanal-Empfangsmodul Funk-Schaltsteckdosen aller Art zu schalten.
+
+![Schaltung: HM Modul, RF Modul, Arduino, Antennen](2015-02-20_15.51.15.jpg "fertiger Aufbau")
+
+Das Projekt basiert auf:
+* Funk-Schaltsteckdosen
+  * bspw. 3er-Set mit Fernbedienung für 10€ in jedem Baumarkt
+  * aus diversen Gründen sind Module vom "Typ A" zu empfehlen [10-fach DIP-Schalter](http://rc-switch.googlecode.com/svn/wiki_images/typeA.png)
+* HomeMatic 8-Kanal-Empfangsmodul
+  * http://www.elv.de/homematic-8-kanal-empfangsmodul.html
+  * ~20€
+* Arduino Nano
+  * http://arduino.cc/en/Main/ArduinoBoardNano
+  * bspw. http://www.ebay.de/itm/301292188973
+  * USB-Controller-Treiber für günstigen China-Clone: http://www.5v.ru/ch340g.htm
+  * ~4€
+* 433 MHz Transmitter
+  * bspw. http://www.ebay.de/itm/221652085232
+  * rc-switch Bibliothek: http://code.google.com/p/rc-switch
+  * ~3€ 
---
+++ homematic-rcswitch-adapter.ino
@@ -0,0 +1,81 @@
+// off-topic: http://www.5v.ru/ch340g.htm
+
+//#define DEBUG
+#ifdef DEBUG
+  #define DEBUG_PRINT(x)     Serial.print (x)
+  #define DEBUG_PRINTDEC(x)     Serial.print (x, DEC)
+  #define DEBUG_PRINTLN(x)  Serial.println (x)
+#else
+  #define DEBUG_PRINT(x)
+  #define DEBUG_PRINTDEC(x)
+  #define DEBUG_PRINTLN(x) 
+#endif 
+
+
+// HM-MOD-Re-8 HomeMatic 8-Kanal-Empfangsmodul 
+// http://www.elv.de/homematic-8-kanal-empfangsmodul.html
+const int OUT1pin = 2; //first OUT pin, others following 
+const int channelCount = 8;
+int channelStates[channelCount] = {-1, -1, -1, -1, -1, -1, -1, -1}; // invalid value (neither HIGH nor LOW), will force full update on startup
+
+
+// operate remote radio controlled devices with 433MHz AM transmitter
+// http://code.google.com/p/rc-switch
+#include <RCSwitch.h>
+RCSwitch rfModule = RCSwitch();
+const int rfModulePin = 13;
+char group[6] = "01110";
+char sockets[8][6]={
+  "00001",
+  "00010",
+  "00100",
+  "01000",
+  "10000",
+  "00011",
+  "00110",
+  "01100",
+};
+
+//temporary variable
+int newState;
+
+
+void setup() {
+  #ifdef DEBUG
+  Serial.begin(9600);
+  #endif
+  
+  for (int pin=OUT1pin; pin < OUT1pin + channelCount; pin++){
+    pinMode(pin, INPUT_PULLUP);
+  }
+  
+  rfModule.enableTransmit(rfModulePin);
+}
+
+
+void loop() {
+  for (int i=0; i < channelCount; i++){
+    newState = !digitalRead(OUT1pin + i);
+    if (newState != channelStates[i]) {
+      channelStates[i] = newState;
+      
+      DEBUG_PRINT("Homematic Channel "); DEBUG_PRINT(i + 1);
+      DEBUG_PRINT(" new State "); DEBUG_PRINT(newState);
+      DEBUG_PRINT(" -> "); DEBUG_PRINT(sockets[i]);
+      
+      if (newState == HIGH) {
+        rfModule.switchOn(group, sockets[i]);
+        DEBUG_PRINT(" switchOn");
+      } else {
+        rfModule.switchOff(group, sockets[i]);
+        DEBUG_PRINT(" switchOff");
+      }
+      DEBUG_PRINT("\n");
+    }
+  }
+}
+
+
+
+
+
